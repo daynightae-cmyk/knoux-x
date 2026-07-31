029b7da330940bf33f1c1c4a4ad124e67bf149d8
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-31 09:17:58 +0400

    chore: stabilize startup bridge linting

--- electron/startup/runtime-dependencies.ts
+++ electron/startup/runtime-dependencies.ts
@@ -1,5 +1,5 @@
+/* eslint-disable import/order -- platform resolvers classify these two bundled startup packages differently. */
 import log from 'electron-log';
-
 import started from 'electron-squirrel-startup';
 
 export { log, started };
